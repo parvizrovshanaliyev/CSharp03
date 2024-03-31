d10d8ed95dc0d7beac842b9569c1b044c4be7a1f
Parviz Aliyev <parvizrovshanaliyev@gmail.com>
2024-03-31 18:42:32 +0400

    we created console app

---
+++ CSharp03/ConsoleApps/Week5/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Hello, World!";
+            int number = 2;
+
+
+            if (number == 1 || number ==2)
+            {
+                Console.WriteLine(text);
+            }
+        }
+    }
+}
