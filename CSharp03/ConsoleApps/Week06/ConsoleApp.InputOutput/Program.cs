42e2e6fa40b39dc71ba92682ff2abdebafc48dbc
Parviz Aliyev <parvizrovshanaliyev@gmail.com>
2024-04-13 14:01:09 +0400

    print the sum of two numbers

--- CSharp03/ConsoleApps/Week06/ConsoleApp.InputOutput/Program.cs
+++ CSharp03/ConsoleApps/Week06/ConsoleApp.InputOutput/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            
+
 
             // print the sum of two numbers
             int number1 = 5;
